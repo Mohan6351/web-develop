6c9d56b0c8aa1853d4dbc7d16fd6ec6b7b135b04
Mohan6351 <mohankumarho2003@gmail.com>
2024-05-15 15:00:19 +0530

    hi

---
+++ nar/script.js
@@ -0,0 +1,13 @@
+function validateage(){
+   var age = document.getElementById("age").value;
+   var result=document.querySelector("p");
+
+    if(age>=18)
+        {
+        result.innerHTML="you are eligible to vote";
+        }
+    else{
+    
+        result.innerHTML="you are still child😂😂😂😂";
+    }
+}
